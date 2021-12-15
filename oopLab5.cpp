29035dad95b11f1bf4219cad257b8374a501f862
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 23:36:55 +0500

    comm

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -47,26 +47,44 @@ void func3(Base& obj) {
 Base func1() {
 	cout << "Base func()\n";
 	Base obj;
-	return obj; // &obj;
+	return obj; // or return it on adress &obj
+
+	//This way of return allows to work easily with static variabels and simple objects.
+	//But when we need to return arrays, pointers or complex objects we'd better use functions,..
+	//..that will return adresses or pointers.
 };
 
 Base* func2() {
 	cout << "Base* func()\n";
-	Base* obj;
+	Base* obj = nullptr;
 	return obj;
 
 	/*
-	Base obj;
-	return &obj;		- our obj will be deleted after the work of a function so we'll get a pointer to uninitialized memory
-						Pointer that will get this adress will be called a hanging pointer (висячий указатель)
+	Base obj;			If we create a static object here and will try to return it we'll face the problem.
+	return &obj;		Our obj will be deleted after the work of a function so we'll get a pointer to uninitialized memory.
+						Pointer that will get this adress will be called a hanging pointer (висячий указатель).
 	*/
+
+	//This way of returning allows to return an adress on the dynamically initialized memory.
+	//But if we want to work with static objects and return some of them, we need to be careful..
+	//..and avoid the creating of hanging pointers.
 };
 
 Base& func3() {
 	cout << "Base& func()\n";
 	Base obj;
-	return obj;					
+	return obj;			// obj returns by an adress.
+						// But by the end of a field of view of this function object obj will be deleted..
+						// And the pointer that will get this adress will be the hanging pointer.
 };								
+Base& better_func3(Base& obj) {
+	cout << "Base& finc(Base& obj)\n";
+	return obj;			// And here we won't get in the result any hanging pointers.
+
+	//This way of return also allows to return an adress on the dynamically initialized memory.
+	//It also have the same problems with hanging pointers.
+	//We should use this way of return with getting some parameters as adresses.
+}
 
 int main()
 {
