b3dda9331c5200fe80946080691db808f580bd13
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 14:00:07 +0500

    initcomm

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -3,56 +3,26 @@
 using namespace std;
 class Point {
 public:
-	string overrided() {
-		return "(overrided): Point";
-	}
-	virtual string overlappedBase() {
-		return "(overlapped in Point): Point";
-	}
-	string overlappedDesc() {
-		return "(overlapped in Point2D): Point";
-	}
+	
 	Point() {
 	}
-	~Point() {
+	virtual ~Point() {
+		cout << "~Point\n";
 	}
 };
-class Point2D : Point {
+class Point2D : public Point {
 public:
-	string overrided() {
-		return "(overrided): Point2D";
-	}
-	string overlappedBase() {
-		return "(overlapped in Point): Point2D";
-	}
-	virtual string overlappedDesc() {
-		return "(overlapped in Point2D): Point2D";
-	}
+	
 	Point2D(){
 	}
 	~Point2D() {
-	}
-};
-class Point3D : Point2D {
-public:
-	string overrided() {
-		return "(overrided): Point3D";
-	}
-	string overlappedBase() {
-		return "(overlapped in Point): Point3D";
-	}
-	virtual string overlappedDesc() {
-		return "(overlapped in Point2D): Point3D";
-	}
-	Point3D() {
-	}
-	~Point3D() {
+		cout << "~Point2D\n";
 	}
 };
 
 int main()
 {
-
+	
 
 	return 0;
 }
