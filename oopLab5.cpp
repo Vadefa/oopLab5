e36b8ef2dc43d93a5a2c9aacb3e22cc27db12ba7
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 14:27:32 +0500

    comm

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -4,7 +4,16 @@ using namespace std;
 class Point {
 public:
 	
+	void method1() {
+		cout << "Point: Method1()\n";
+		method2();
+	}
+	/*virtual */void method2() {
+		cout << "Point: Method2()\n";
+	}
+
 	Point() {
+		cout << "Point()\n";
 	}
 	virtual ~Point() {
 		cout << "~Point\n";
@@ -12,8 +21,12 @@ public:
 };
 class Point2D : public Point {
 public:
-	
+	void method2() {
+		cout << "Point2d: Method2();\n";
+	}
+
 	Point2D(){
+		cout << "Point2d()\n";
 	}
 	~Point2D() {
 		cout << "~Point2D\n";
@@ -22,7 +35,8 @@ public:
 
 int main()
 {
-	
+	Point2D pointer2d;
+	pointer2d.method1();
 
 	return 0;
 }
