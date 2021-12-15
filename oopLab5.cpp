8678c906b1c52dfe504e5c32840b700e5f29f992
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 14:11:38 +0500

    com

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -3,50 +3,38 @@
 using namespace std;
 class Point {
 public:
-	string overrided() {
-		return "(overrided): Point";
+	void overrided() {
+		cout << "(overrided): Point";
 	}
-	virtual string overlappedBase() {
-		return "(overlapped in Point): Point";
+	virtual void overlappedBase() {
+		cout << "(overlapped in Point): Point";
 	}
-	string overlappedDesc() {
-		return "(overlapped in Point2D): Point";
+	void overlappedDesc() {
+		cout << "(overlapped in Point2D): Point";
 	}
+
 	Point() {
 	}
 	virtual ~Point() {
+		cout << "~Point\n";
 	}
 };
 class Point2D : Point {
 public:
-	string overrided() {
-		return "(overrided): Point2D";
+	void overrided() {
+		cout << "(overrided): Point2D";
 	}
-	string overlappedBase() {
-		return "(overlapped in Point): Point2D";
+	void overlappedBase() {
+		cout << "(overlapped in Point): Point2D";
 	}
-	virtual string overlappedDesc() {
-		return "(overlapped in Point2D): Point2D";
+	void overlappedDesc() {
+		cout << "(overlapped in Point2D): Point2D";
 	}
+
 	Point2D(){
 	}
 	~Point2D() {
-	}
-};
-class Point3D : Point2D {
-public:
-	string overrided() {
-		return "(overrided): Point3D";
-	}
-	string overlappedBase() {
-		return "(overlapped in Point): Point3D";
-	}
-	virtual string overlappedDesc() {
-		return "(overlapped in Point2D): Point3D";
-	}
-	Point3D() {
-	}
-	~Point3D() {
+		cout << "~Point2D\n";
 	}
 };
 
