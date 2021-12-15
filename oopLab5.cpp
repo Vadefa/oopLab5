3a98ab41fd6404a8603930246e4e1287ccd5450b
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 13:27:33 +0500

    Добавьте файлы проекта.

---
+++ oopLab5.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+class Point {
+public:
+	string overrided() {
+		return "(overrided): Point";
+	}
+	virtual string overlappedBase() {
+		return "(overlapped in Point): Point";
+	}
+	string overlappedDesc() {
+		return "(overlapped in Point2D): Point";
+	}
+	Point() {
+	}
+	~Point() {
+	}
+};
+class Point2D : Point {
+public:
+	string overrided() {
+		return "(overrided): Point2D";
+	}
+	string overlappedBase() {
+		return "(overlapped in Point): Point2D";
+	}
+	virtual string overlappedDesc() {
+		return "(overlapped in Point2D): Point2D";
+	}
+	Point2D(){
+	}
+	~Point2D() {
+	}
+};
+class Point3D : Point2D {
+public:
+	string overrided() {
+		return "(overrided): Point3D";
+	}
+	string overlappedBase() {
+		return "(overlapped in Point): Point3D";
+	}
+	virtual string overlappedDesc() {
+		return "(overlapped in Point2D): Point3D";
+	}
+	Point3D() {
+	}
+	~Point3D() {
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
