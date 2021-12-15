6e1ee4603c1758132413563b13d8470239a7aa30
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-15 14:16:18 +0500

    com

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -4,34 +4,36 @@ using namespace std;
 class Point {
 public:
 	void overrided() {
-		cout << "(overrided): Point";
+		cout << "(overrided): Point\n";
 	}
 	virtual void overlappedBase() {
-		cout << "(overlapped in Point): Point";
+		cout << "(overlapped in Point): Point\n";
 	}
 	void overlappedDesc() {
-		cout << "(overlapped in Point2D): Point";
+		cout << "(overlapped in Point2D): Point\n";
 	}
 
 	Point() {
+		cout << "Point()\n";
 	}
 	virtual ~Point() {
 		cout << "~Point\n";
 	}
 };
-class Point2D : Point {
+class Point2D : public Point {
 public:
 	void overrided() {
-		cout << "(overrided): Point2D";
+		cout << "(overrided): Point2D\n";
 	}
 	void overlappedBase() {
-		cout << "(overlapped in Point): Point2D";
+		cout << "(overlapped in Point): Point2D\n";
 	}
 	void overlappedDesc() {
-		cout << "(overlapped in Point2D): Point2D";
+		cout << "(overlapped in Point2D): Point2D\n";
 	}
 
 	Point2D(){
+		cout << "Point2D()\n";
 	}
 	~Point2D() {
 		cout << "~Point2D\n";
@@ -40,7 +42,26 @@ public:
 
 int main()
 {
+	cout << "The first case: creating a pointer of a Base class, object of a Base class:\n";
+	Point* pointer = new Point;
+	pointer->overrided();
+	pointer->overlappedBase();
+	pointer->overlappedDesc();
+	delete pointer;
 
+	cout << "The second case: creating a pointer of a Descendant class, object of a Descendant class:\n";
+	Point2D* pointer2D = new Point2D;
+	pointer2D->overrided();
+	pointer2D->overlappedBase();
+	pointer2D->overlappedDesc();
+	delete pointer2D;
+
+	cout << "The second case: creating a pointer of a Base class, object of a Descendant class:\n";
+	Point* p = new Point2D;
+	p->overrided();
+	p->overlappedBase();
+	p->overlappedDesc();
+	delete p;
 
 	return 0;
 }
