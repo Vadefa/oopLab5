8deee56d9570171cba6a438e6e450255407bc568
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-16 14:10:14 +0500

    comm

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -26,6 +26,11 @@ int main()
 		unique_ptr<Base> up2(new Base(obj));
 		unique_ptr<Base> up3(new Base(&obj));
 		//unique_ptr<Base> up4(up1);			- unique pointers can't copy yourself
+
+		unique_ptr<Base> up4;
+		cout << "up1 = " << up1 << "\nup4 = " << up4 << ", making move:\n";
+		up4 = move(up1);
+		cout << "up1 = " << up1 << "\nup4 = " << up4 << "\n";
 	}
 	cout << "\n";
 
@@ -37,6 +42,7 @@ int main()
 		shared_ptr<Base> sp4(sp1);
 		cout << "sp1 = " << sp1 << "\nsp4 = " << sp4 << "\n";
 	}
+	cout << "\n";
 
 	return 0;
 }
