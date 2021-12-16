8502bcd4a32ba58d29b8cd4059ff4e22870a9e92
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-16 14:30:12 +0500

    com

--- oopLab5.cpp
+++ oopLab5.cpp
@@ -67,13 +67,11 @@ int main()
 	for (int i = 0; i < n; i++)
 		if (p[i]->classname() == "Line")					// this breaks the incapsulation rule
 			((Line*)p[i])->eraseLine();						// and this is unsafe type conversion
-
 	cout << "\n";
 
 	for (int i = 0; i < n; i++)
 		if (p[i]->isA("Section"))
 			((Section*)p[i])->eraseSection();				// this is still not so safe, but better then before
-
 	cout << "\n";
 
 	for (int i = 0; i < n; i++) {
@@ -82,7 +80,6 @@ int main()
 		else if (dynamic_cast<Section*>(p[i]) != NULL)
 			dynamic_cast<Section*>(p[i])->eraseSection();	// and this is the safe type conversion
 	}
-
 	cout << "\n";
 
 	return 0;
